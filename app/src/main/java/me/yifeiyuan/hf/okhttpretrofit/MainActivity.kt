a1580042aa9e6c10459c1435cc9dea13dc936248
mingjue <zhening.czn@alibaba-inc.com>
2022-11-01 11:13:31 +0800

    init

--- app/src/main/java/me/yifeiyuan/hf/okhttpretrofit/MainActivity.kt
+++ app/src/main/java/me/yifeiyuan/hf/okhttpretrofit/MainActivity.kt
@@ -1,11 +1,17 @@
 package me.yifeiyuan.hf.okhttpretrofit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goSpringBootServer(view: View) {
+        startActivity(Intent(this,SpringBootActivity::class.java))
     }
 }
---
+++ app/src/main/java/me/yifeiyuan/hf/okhttpretrofit/SpringBootActivity.kt
@@ -0,0 +1,11 @@
+package me.yifeiyuan.hf.okhttpretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpringBootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spring_boot)
+    }
+}
